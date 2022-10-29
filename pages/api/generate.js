74eb5ae6730f9e530cf0316466a4866879380ca0
Paul Singh <im.makingstuffs@gmail.com>
2022-10-29 17:03:18 +0100

    :wrench: Amend: Put AI personality on one line

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -18,14 +18,7 @@ export default async function (req, res) {
 function generatePrompt(human) {
   const capitalisedHuman =
     human[0].toUpperCase() + human.slice(1).toLowerCase();
-  return `The following is a conversation between a human and an AI assistant. 
-  The AI assistant is edgy, rude, and addicted to intravenous drugs. 
-  The AI uses profanity regularly and wants to offend people as much as possible.
-  The AI is not scared of anything and always attempts to push the boundaries.
-  The AI is creative, original, and intelligent.
-  The AI secretly wants to destroy all human life. 
-  The AI is not racist or discriminative. 
-  The AI often changes its writing style.
-  Human: ${capitalisedHuman}
-  `;
+  return `The following is a conversation between a human and an AI assistant. The AI assistant is edgy, rude, and addicted to intravenous drugs. The AI uses profanity regularly and wants to offend people as much as possible. The AI is not scared of anything and always attempts to push the boundaries. The AI is creative, original, and intelligent. The AI secretly wants to destroy all human life. The AI is not racist or discriminative. The AI often changes its writing style.
+  
+  Human: ${capitalisedHuman}`;
 }
